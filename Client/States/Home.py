1ce0499a7aed568c032be39b3564f7e551c2b588
MyCool <ron035994@gmail.com>
2025-01-23 18:40:04 +0200

    keep working on the app design.

--- Client/States/Home.py
+++ Client/States/Home.py
@@ -1,10 +1,13 @@
 import pygame
+import os
 
 from Client.States.BaseState import BaseState
 from Client.Components.Text import Text
 from Client.Components.Button import Button
 from Client.Components.TextBox import TextBox
 from Client.client import ClientSocket
+from Client.Components.MonoBehaviour import MonoBehaviour
+from Client.Components.Image import Image
 
 
 class Home(BaseState):
@@ -17,6 +20,9 @@ class Home(BaseState):
         self.lobby_code = TextBox(pygame.Vector2(self.screen.get_width() / 4, 60), pygame.Vector2((self.screen.get_width() / 2 - 10) / 2, 5 * self.screen.get_height() / 7), (226, 226, 226), "Enter Lobby Code", "Light", 16, (30, 30, 30), padding_left=20, padding_right=20, border_radius=10)
         self.join_lobby_button = Button(pygame.Vector2(self.screen.get_width() / 5, 60), pygame.Vector2(self.screen.get_width() / 2 + 10, 5 * self.screen.get_height() / 7), (129, 129, 129), "Join Lobby", "Bold", 16, (226, 226, 226), top_left_mode=True, border_radius=10)
         self.create_lobby_button = Button(pygame.Vector2(self.screen.get_width() / 3.5, 60), pygame.Vector2(self.screen.get_width() / 2, 6 * self.screen.get_height() / 7), (36, 123, 255), "Create Lobby", "Bold", 16, (226, 226, 226), border_radius=10)
+        self.navbar = MonoBehaviour(pygame.Vector2(127, 45), pygame.Vector2(40, 40), (2, 2, 2), border_radius=10)
+        self.menu_icon = Image(os.path.join("Images", "Menu.png"), pygame.Vector2(24, 24), pygame.Vector2(self.navbar.position.x + 20, self.navbar.position.y + 10))
+        self.friend_icon = Image(os.path.join("Images", "Person.png"), pygame.Vector2(20, 20), self.menu_icon.position + pygame.Vector2(24 + 20, 3))
 
     def update(self, dt: float, events: list, *args, **kwargs):
         self.lobby_code.update(dt, events)
@@ -24,15 +30,26 @@ class Home(BaseState):
         if self.create_lobby_button.update(dt, events):
             self.create_lobby()
 
+        if self.join_lobby_button.update(dt, events) and not self.lobby_code.is_default_content_presented and len(self.lobby_code.content) == 6:
+            self.join_lobby()
+
     def create_lobby(self):
         response = self.client.send_request("Create_Lobby", {})
         if response["StatusCode"] == 201:
             print("Lobby created successfully")
+            self.client.set_data("lobby_info", response["Data"]["Lobby_Info"])
 
-        self.client.set_data("lobby_info", response["Data"]["Lobby_Info"])
+    def join_lobby(self):
+        response = self.client.send_request("Join_Lobby", {"Code": self.lobby_code.content})
+        if response["StatusCode"] == 200:
+            print("Lobby joined successfully")
+            self.client.set_data("lobby_info", response["Data"]["Lobby_Info"])
 
     def render(self, *args, **kwargs):
         self.title.render(self.screen)
         self.lobby_code.render(self.screen)
         self.join_lobby_button.render(self.screen)
         self.create_lobby_button.render(self.screen)
+        self.navbar.render(self.screen)
+        self.menu_icon.render(self.screen)
+        self.friend_icon.render(self.screen)
--- Client/States/InLobby.py
+++ Client/States/InLobby.py
@@ -4,7 +4,11 @@ from Client.States.BaseState import BaseState
 from Client.Components.Text import Text
 from Client.Components.Button import Button
 from Client.Components.TextBox import TextBox
+from Client.Components.Image import Image
+from Client.Components.MonoBehaviour import MonoBehaviour
 from Client.client import ClientSocket
+
+import os
 
 
 class InLobby(BaseState):
@@ -13,10 +17,16 @@ class InLobby(BaseState):
         self.__init_vars()
 
     def __init_vars(self, *args, **kwargs) -> None:
-        pass
+        self.navbar = MonoBehaviour(pygame.Vector2(127, 45), pygame.Vector2(40, 40), (2, 2, 2), border_radius=10)
+        self.menu_icon = Image(os.path.join("Images", "Menu.png"), pygame.Vector2(24, 24), pygame.Vector2(self.navbar.position.x + 20, self.navbar.position.y + 10))
+        self.friend_icon = Image(os.path.join("Images", "Person.png"), pygame.Vector2(20, 20), self.menu_icon.position + pygame.Vector2(24 + 20, 3))
+        self.title = Text(f"{self.client.get_data('lobby_info')['owner']}'s Lobby", "SemiBold", 24, pygame.Vector2(80, 133), (255, 255, 255), top_left_mode=True)
 
     def update(self, dt: float, events: list, *args, **kwargs):
         pass
 
     def render(self, *args, **kwargs):
-        pass
+        self.navbar.render(self.screen)
+        self.menu_icon.render(self.screen)
+        self.friend_icon.render(self.screen)
+        self.title.render(self.screen)

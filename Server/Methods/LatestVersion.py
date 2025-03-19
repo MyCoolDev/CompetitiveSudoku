8bdea295e511fd67eaa1a36c7d7d499c1cca767f
MyCool <ron035994@gmail.com>
2025-03-19 18:37:19 +0200

    working on lobby system

---
+++ Server/Methods/LatestVersion.py
@@ -0,0 +1,6 @@
+"""
+This file is importing the latest version of the Methods.
+"""
+
+import Server.Methods.v1.UserSystem as account
+import Server.Methods.v1.FriendSystem as friend
--- Server/ServerSocket.py
+++ Server/ServerSocket.py
@@ -3,7 +3,7 @@ import random
 import socket
 import string
 
-import API.LatestVersion as api
+import Methods.LatestVersion as api
 import Hashing
 import utils
 from ClientInterface import Client
